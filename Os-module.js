a3d8e8bc87ce416bfb20e8e507d7540391220690
sjt-wtag <sumaya.jahan@welldev.io>
2025-02-16 15:55:14 +0600

    Os module

---
+++ Os-module.js
@@ -0,0 +1,10 @@
+const os = require("os");
+console.log(os.userInfo());
+
+console.log("os.totalmem()", os.totalmem());
+
+console.log("os.freemem()", os.freemem());
+console.log("os.freemem()", os.hostname());
+console.log("os.freemem()", os.platform());
+
+console.log("os.cpus()", os.cpus());
